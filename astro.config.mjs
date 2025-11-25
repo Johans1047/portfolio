44865ada8d167c98c10c70c55089ec4056a20d5c
Jonathan Salazar <jonyalex1047@gmail.com>
2025-11-24 22:41:01 -0500

    fix: resolver aliases imports on Vercel v1.1.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@i18n': path.resolve(__dirname, './src/i18n')
+      }
+    }
   },
 });
